6ad68e33e968a1d8537b4d9a97db33f75959c3cb
yus <swiftcase@gmail.com>
2026-02-07 20:31:58 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,40 @@ module.exports = function(eleventyConfig) {
   
   // Pass through CSS
   eleventyConfig.addPassthroughCopy("css");
+  // Create tag collection
+  eleventyConfig.addCollection("tagList", function(collectionApi) {
+    let tagSet = new Set();
+    collectionApi.getAll().forEach(item => {
+      if ("tags" in item.data) {
+        let tags = item.data.tags;
+        if (typeof tags === "string") {
+          tags = [tags];
+        }
+        tags.forEach(tag => tagSet.add(tag));
+      }
+    });
+    return Array.from(tagSet).sort();
+  });
+  
+  // Create pages for each tag
+  eleventyConfig.addCollection("postsByTag", function(collectionApi) {
+    let tagMap = {};
+    collectionApi.getAll().forEach(item => {
+      if ("tags" in item.data) {
+        let tags = item.data.tags;
+        if (typeof tags === "string") {
+          tags = [tags];
+        }
+        tags.forEach(tag => {
+          if (!tagMap[tag]) {
+            tagMap[tag] = [];
+          }
+          tagMap[tag].push(item);
+        });
+      }
+    });
+    return tagMap;
+  });
   
   return {
     dir: {
