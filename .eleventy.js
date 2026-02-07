f85e19b20eba704cc7a96bfa744e62df23930b2f
yus <swiftcase@gmail.com>
2026-02-07 20:47:34 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,47 +1,17 @@
-const pluginTailwind = require("eleventy-plugin-tailwindcss");
-
 module.exports = function(eleventyConfig) {
-  // Add Tailwind plugin
-  eleventyConfig.addPlugin(pluginTailwind, {
-    src: "css/tailwind.css",
-    keepFolderStructure: false,
-  });
+  // No plugin needed! Just passthrough CSS
   
-  // Pass through CSS
-  eleventyConfig.addPassthroughCopy("css");
-  // Create tag collection
+  // Tag system
   eleventyConfig.addCollection("tagList", function(collectionApi) {
     let tagSet = new Set();
     collectionApi.getAll().forEach(item => {
       if ("tags" in item.data) {
         let tags = item.data.tags;
-        if (typeof tags === "string") {
-          tags = [tags];
-        }
+        if (typeof tags === "string") tags = [tags];
         tags.forEach(tag => tagSet.add(tag));
       }
     });
     return Array.from(tagSet).sort();
-  });
-  
-  // Create pages for each tag
-  eleventyConfig.addCollection("postsByTag", function(collectionApi) {
-    let tagMap = {};
-    collectionApi.getAll().forEach(item => {
-      if ("tags" in item.data) {
-        let tags = item.data.tags;
-        if (typeof tags === "string") {
-          tags = [tags];
-        }
-        tags.forEach(tag => {
-          if (!tagMap[tag]) {
-            tagMap[tag] = [];
-          }
-          tagMap[tag].push(item);
-        });
-      }
-    });
-    return tagMap;
   });
   
   return {
