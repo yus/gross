856224cdd12602b1107ed4902c0bc6579da851d1
yus <swiftcase@gmail.com>
2026-02-07 20:29:43 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,15 @@
+const pluginTailwind = require("eleventy-plugin-tailwindcss");
+
 module.exports = function(eleventyConfig) {
+  // Add Tailwind plugin
+  eleventyConfig.addPlugin(pluginTailwind, {
+    src: "css/tailwind.css",
+    keepFolderStructure: false,
+  });
+  
+  // Pass through CSS
+  eleventyConfig.addPassthroughCopy("css");
+  
   return {
     dir: {
       input: ".",
